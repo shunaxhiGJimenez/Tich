fcf912d4b6af1610d595cd56c63d74d1b2969cbe
shunaxhiGJimenez <91807589+shunaxhiGJimenez@users.noreply.github.com>
2023-03-18 10:57:43 -0600

    Ejercicio de Arreglos
    Es un ejercicio de arreglos

---
+++ Arreglos/Arreglos/ArregloNumeros.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arreglos
+{
+    public class ArregloNumeros
+    {
+        private int[] numeros;
+        private int cantidadNumeros;
+
+
+        public ArregloNumeros(int tamMaximo)
+        {
+            numeros = new int[tamMaximo];
+            cantidadNumeros = 0;
+        }
+
+        public void AgregarNumero(int numero)
+        {
+            if (cantidadNumeros < numeros.Length)
+            {
+                numeros[cantidadNumeros] = numero;
+                cantidadNumeros++;
+            }
+            else
+            {
+                Console.WriteLine("El arreglo esta lleno, no se pueden agregar mas numeros ");
+            }
+        }
+
+        public double calcularPromedio()
+        {
+            if (cantidadNumeros > 0)
+            {
+                int suma = calcularSumaRecursiva(0);
+                return (double)suma / cantidadNumeros;
+            }
+            else
+            {
+                Console.WriteLine("El arreglo esta vacio, no se puede calcular el promedio");
+                return 0;
+            }
+        }
+
+        public int calcularSuma()
+        {
+            if (cantidadNumeros > 0)
+            {
+                return calcularSumaRecursiva(0);
+            }
+            else
+            {
+                Console.WriteLine("El arreglo esta vacio, no se puede calcular el promedio");
+                return 0;
+            }
+        }
+
+        public int calcularSumaRecursiva(int indice)
+        {
+            if (indice == cantidadNumeros -1)
+            {
+                return numeros[indice];
+            }
+            else
+            {
+                return numeros[indice] + calcularSumaRecursiva(indice + 1);
+            }
+        }
+
+        public double CalcularMedia()
+        {
+            if (cantidadNumeros > 0)
+            {
+                Array.Sort(numeros, 0, cantidadNumeros);
+                if (cantidadNumeros % 2 == 0)
+                {
+                    int indiceMedio1 = cantidadNumeros / 2 - 1;
+                    int indiceMedio2 = cantidadNumeros / 2;
+                    return (double)(numeros[indiceMedio1] + numeros[indiceMedio2]) / 2;
+                }
+                else
+                {
+                    int indiceMedio = cantidadNumeros / 2;
+                    return numeros[indiceMedio];
+                }
+            }
+            else
+            {
+                Console.WriteLine("El arreglo esta vacio, no se puede calcular el promedio");
+                return 0;
+            }
+        }
+
+        public void separaNumerosPositivosNegativos(out int[] numerosPositivos, out int[] numerosNegativos)
+        {
+            int cantidadPositivos = 0;
+            int cantidadNegativos = 0;
+
+            for (int i = 0; i < cantidadNumeros; i++)
+            {
+                if (numeros[i] >= 0)
+                {
+                    cantidadPositivos++;
+                }
+                else
+                {
+                    cantidadNegativos++;
+                }
+            }
+
+            numerosPositivos = new int[cantidadNumeros];
+            numerosNegativos = new int[cantidadNegativos];
+
+            int indicePositivos = 0;
+            int indiceNegativos = 0;
+
+            for (int i = 0; i < cantidadNumeros; i++)
+            {
+                if (numeros[i] >= 0)
+                {
+                    numerosPositivos[indicePositivos] = numeros[i];
+                    indicePositivos++;
+                }
+            }
+        }
+
+
+    }
+}
---
+++ Arreglos/Arreglos/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arreglos
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+           
+
+            Console.WriteLine("Ingrese el tamaño del arreglo: ");
+            int tam = int.Parse(Console.ReadLine());
+            ArregloNumeros arr = new ArregloNumeros(tam);
+
+            Console.WriteLine("Ingrese al menos 10 numeros enteros (entre -1500 y 3650)");
+
+            bool entradaValida = false;
+
+            while (!entradaValida)
+            {
+                string entrada = Console.ReadLine();
+                string[] numerosStr = entrada.Split(' ');
+
+                if (numerosStr.Length < 10)
+                {
+                    Console.WriteLine("Debe ingresar al menos 10 numeros, vuelva a intentar");
+                    continue;
+                }
+
+                entradaValida= true;
+                foreach (string str in numerosStr)
+                {
+                    int numero;
+                    if (int.TryParse(str, out numero))
+                    {
+                        if (numero < -1500 || numero > 36500)
+                        {
+                            Console.WriteLine($"El numero {numero} esta fuera del rango permitido. Vuelva a intentar.");
+                            entradaValida = false;
+                            break;
+                        }
+                        else
+                        {
+                            arr.AgregarNumero(numero);
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine($"El valor {str} no es un numero entero valido. vuelva a intentar. ");
+                        entradaValida = false;
+                        break;
+                    }
+                }
+            }
+
+            int[] numerosPositivos;
+            int[] numerosNegativos;
+            arr.separaNumerosPositivosNegativos(out numerosPositivos, out numerosNegativos);
+
+            Console.WriteLine("numeros positivos: " + string.Join(", ", numerosPositivos));
+            Console.WriteLine("numeros negativos: " + string.Join(", ", numerosNegativos));
+            Console.WriteLine($"Promedio: {arr.calcularPromedio()}");
+            Console.WriteLine($"Media: {arr.CalcularMedia()}");
+            Console.WriteLine($"Suma Total: {arr.calcularSuma()}");
+
+            Console.ReadLine();
+
+        }
+    }
+}
